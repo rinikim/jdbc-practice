31d564c75b9bfbb7e174bdfa9029666074dc76b5
rinikim <tytntu@naver.com>
2022-11-09 19:45:09 +0900

    refactor : ConnectionManager 리팩터링
    - db connection 을 모아둔 ConnectionManager class 리팩터링

--- src/main/java/org/example/UserDao.java
+++ src/main/java/org/example/UserDao.java
@@ -3,24 +3,13 @@ package org.example;
 import java.sql.*;
 
 public class UserDao {
-    private Connection getConnection() {
-        String url = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
-        String id = "sa";
-        String pw = "";
 
-        try {
-            Class.forName("org.h2.Driver");
-            return DriverManager.getConnection(url, id, pw);
-        } catch (Exception ex) {
-            return null;
-        }
-    }
     public void create(User user) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
 
         try {
-            con = getConnection();
+            con = ConnectionManager.getConnection();
             String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1, user.getUserId());
@@ -47,7 +36,7 @@ public class UserDao {
 
         // try with resource 를 사용하면 자원을 자동으로 해제해준다.
         try {
-            con = getConnection();
+            con = ConnectionManager.getConnection();
             String sql = "SELECT userId, password, name, email FROM USERS WHERE userId = ?";
             pstmt = con.prepareStatement(sql);
             pstmt.setString(1, userId);
